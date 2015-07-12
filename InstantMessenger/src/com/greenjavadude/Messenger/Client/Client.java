332dd6d26e6affff00d5ce7c99e1b4897a46f9a6
Green Java Dude <greenjavadude@gmail.com>
2015-07-12 17:04:14 +0200

    WIP

--- InstantMessenger/src/com/greenjavadude/Messenger/Client/Client.java
+++ InstantMessenger/src/com/greenjavadude/Messenger/Client/Client.java
@@ -1,5 +1,5 @@
 package com.greenjavadude.Messenger.Client;
 
 public class Client {
-
+	
 }
--- InstantMessenger/src/com/greenjavadude/Messenger/Server/Server.java
+++ InstantMessenger/src/com/greenjavadude/Messenger/Server/Server.java
@@ -1,5 +1,67 @@
 package com.greenjavadude.Messenger.Server;
 
-public class Server {
+import java.awt.BorderLayout;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import javax.swing.*;
+
+public class Server extends JFrame implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
+	private boolean running;
+	
+	private JTextArea area;
+	
+	public Server(){
+		super("Messenger Server");
+		
+		running = false;
+		
+		area.setEditable(false);
+		
+		add(new JScrollPane(area), BorderLayout.CENTER);
+		
+		setSize(500, 300);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void run(){
+		try{
+			while(running){
+				//stay available, it's a server
+			}
+		}catch(Exception e){
+			
+		}
+	}
+	
+	public void start(){
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop(){
+		running = false;
+		System.exit(0);
+	}
+	
+	public static void main(String[] args){
+		Server server = new Server();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
